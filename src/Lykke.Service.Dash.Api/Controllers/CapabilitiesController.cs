62d52b1123da424f1be535c0170b19bb7e3abc89
Andrey Krivoshapov <andrey.krivoshapov@lykke.com>
2018-01-19 13:53:51 +0300

    Add Capabilities controller

---
+++ src/Lykke.Service.Dash.Api/Controllers/CapabilitiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lykke.Service.Dash.Api.Controllers
+{
+    [Route("api/capabilities")]
+    public class CapabilitiesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var capabilities = new
+            {
+                isTransactionsRebuildingSupported = false,
+                isBatchedTransactionsSupported = false
+            };
+
+            return Ok(capabilities);
+        }
+    }
+}
